9979e474c71845a5e0e8524acfcf369066c94d81
Drew W <20048187+dvvilkins@users.noreply.github.com>
2024-09-23 17:08:24 -0400

    successful test of secrets

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
 port = st.secrets["QDRANT_PORT"]
-st.title("Hello, Streamlit!")
 st.write("This is a test app.")
-st.write("This is a test secret:")
 
+st.write("This is a test secret:")
 st.write(port)
-st.write("This is a test app with an input field.")
 
 # Input field for user input
 user_input = st.text_input("Enter some text:")
--- tests/main_test.py
+++ tests/main_test.py
@@ -8,7 +8,7 @@ import main
 def test_main_app():
     # Load the app from the file
     at = AppTest.from_file("main.py")  # Adjust the path to your app file if necessary
-    hi = at.secrets["QDRANT_PORT"]
+    #hi = at.secrets["QDRANT_PORT"]
     
     # Run the app the first time (initial load)
     at.run()
@@ -17,4 +17,4 @@ def test_main_app():
     at.text_input[0].input("Test input from the test script").run()
 
     # Verify the output displayed by the app after the input is processed
-    assert "You entered: Test input" in at.markdown[1].value  # Checking second markdown output
+    assert "You entered: Test input" in at.markdown[3].value  # Checking second markdown output
