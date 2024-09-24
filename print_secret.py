c37694a57a99b28d76d8255048b58bad76304d11
Drew W <20048187+dvvilkins@users.noreply.github.com>
2024-09-24 11:42:11 -0400

    removed unnecessary test file

--- print_secret.py
+++
@@ -1,9 +0,0 @@
-# print_secret.py
-import streamlit as st
-
-# Access and print the secret
-if "QDRANT_API_KEY" in st.secrets:
-    api_key = st.secrets["QDRANT_API_KEY"]
-    st.write(f"QDRANT_API_KEY: {api_key}")
-else:
-    st.write("QDRANT_API_KEY not found in secrets.")
