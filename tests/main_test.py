6bc0c071329915a0a1d68b2fbb0abcf47a6ca795
Drew W <20048187+dvvilkins@users.noreply.github.com>
2024-10-20 14:37:02 -0400

    starting over

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,20 +1,14 @@
 from streamlit.testing.v1 import AppTest
-import sys
-import os
-
-
-import main
 
 def test_main_app():
-    # Load the app from the file
-    at = AppTest.from_file("main.py")  # Adjust the path to your app file if necessary
-    #hi = at.secrets["QDRANT_PORT"]
+    at = AppTest.from_file("main.py", default_timeout=5)
     
-    # Run the app the first time (initial load)
+    # if you need to access secrets, do that here before at.run()
     at.run()
+    assert not at.exception, f"An exception occurred: {at.exception}"
     
+    assert "6333" in at.markdown[2].value # confirm the app accessed st.secrets
     # Simulate user input in the text input widget
-    at.text_input[0].input("Test input from the test script").run()
+    at.text_input[0].input("You are doing it, Drew!").run()
 
-    # Verify the output displayed by the app after the input is processed
-    assert "You entered: Test input" in at.markdown[3].value  # Checking second markdown output
+    assert "You entered: You are doing it, Drew!" in at.markdown[3].value  # Checks second markdown output for value. If this condition is true, the test passes; otherwise, it fails.
